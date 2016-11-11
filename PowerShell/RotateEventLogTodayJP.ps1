772aad9145738968dc4e14691ed2fddf7ea3e961
08thse <08thse@gmail.com>
2016-11-11 18:30:43 +0900

    add PowerShell

---
+++ PowerShell/RotateEventLogTodayJP.ps1
@@ -0,0 +1,5 @@
+$FileName = (Get-Date).AddDays(-1).ToString("yyyy-MM-dd") + ".evtx"
+$StartDay = (Get-Date).AddDays(-2).ToString("yyyy-MM-dd") + "T15:00:00.000Z"
+$EndDay = (Get-Date).AddDays(-1).ToString("yyyy-MM-dd") + "T15:00:00.999Z"
+$QueryString = "*[System[TimeCreated[@SystemTime>=`'" + $StartDay + "`' and @SystemTime<`'" + $EndDay + "`']]]"
+wevtutil epl System $FileName /q:"$QueryString"
