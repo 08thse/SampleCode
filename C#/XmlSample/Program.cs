2973af0964cc5fb20e772c274d676b8501d14e83
08thse <08thse@gmail.com>
2017-05-17 23:47:37 +0900

    add XmlSample

---
+++ C#/XmlSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XmlSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var option = new XElement("option");
+            option.SetElementValue("enabled", true);
+            option.SetElementValue("min", 0);
+            option.SetElementValue("max", 100);
+
+            option.SetAttributeValue("visible", true);
+
+            var root = new XElement("settings", option);
+            root.Save("sample.xml");
+        }
+    }
+}
